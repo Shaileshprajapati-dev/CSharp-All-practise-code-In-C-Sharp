a4462278dfd47cd13aceb4c21fbfe27adfb586e6
Shaileshprajapati-dev <95132308+Shaileshprajapati-dev@users.noreply.github.com>
2025-07-15 20:22:28 +0530

    Add files via upload

---
+++ Array_csharp/Array_csharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] my_array = new int[5];
+            my_array[0] = 11;
+            my_array[1] = 14;
+            my_array[2] = 15;
+            my_array[3] = 30;
+            my_array[4] = 20;
+
+            Console.WriteLine(my_array[0]);
+
+            Console.ReadLine();
+        }
+    }
+}
