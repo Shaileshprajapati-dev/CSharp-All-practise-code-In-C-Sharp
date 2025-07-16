59c089a15e4a89990a1d8a851367623964c13e65
Shaileshprajapati-dev <95132308+Shaileshprajapati-dev@users.noreply.github.com>
2025-07-16 15:27:26 +0530

    Add files via upload

---
+++ Static_And_Instance_Members/Static_And_Instance_Members/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Static_And_Instance_Members
+{
+
+    class Student
+    {
+        public int rollNo;
+        public string firstName;
+        public string lastName;
+        public int standard;
+
+        public void printFullnName() {
+            string fullname = this.firstName + " " + this.lastName;
+            Console.WriteLine(fullname);
+        
+        }
+
+    }
+
+
+    internal class Program
+    {
+       
+      
+        static void Main(string[] args)
+        {
+           
+            Student Shai = new Student();
+            Shai.rollNo= 30;
+            Shai.firstName = "shailesh";
+            Shai.lastName = "prajapati";
+            Shai.standard = 1;
+
+
+            Student pra = new Student();
+            pra.rollNo = 20;
+
+            pra.firstName = "Komal";
+            pra.lastName = "prajapati";
+            pra.standard = 1;
+
+            Console.WriteLine(Shai.rollNo);
+            Console.WriteLine(Shai.firstName);
+            Console.WriteLine(Shai.lastName);
+            Console.WriteLine(Shai.standard);
+            Shai.printFullnName();
+
+            Console.WriteLine(pra.rollNo);
+            Console.WriteLine(pra.firstName);
+            Console.WriteLine(pra.lastName);
+            Console.WriteLine(pra.standard);
+            pra.printFullnName();
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
