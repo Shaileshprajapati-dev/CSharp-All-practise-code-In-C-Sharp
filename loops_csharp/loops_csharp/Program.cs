d1668815742567701e6d7c604e35e09cbb34211a
Shaileshprajapati-dev <95132308+Shaileshprajapati-dev@users.noreply.github.com>
2025-07-16 13:12:52 +0530

    Add files via upload

---
+++ loops_csharp/loops_csharp/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace loops_csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string confrim;
+            do
+            {
+                Console.WriteLine("Enter first Number");
+                int firstNumber = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter second Number");
+                int secondNumber = int.Parse(Console.ReadLine());
+                int add = firstNumber + secondNumber;
+                Console.WriteLine("The sum of {0} and {1} is {2}", add);
+                Console.WriteLine("do you want to continue? (yes/no)");
+                confrim = Console.ReadLine();
+            }
+         while ((confrim == "yes");
+
+            Console.WriteLine("Thank you for using the program!");
+                Console.WriteLine("Press any key to exit...");
+
+            Console.ReadLine();
+        }
+    }
+}
