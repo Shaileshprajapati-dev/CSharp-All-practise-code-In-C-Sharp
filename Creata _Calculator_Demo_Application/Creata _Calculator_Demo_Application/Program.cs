2e8f78495a1d6bb8a6e6d2e8cc77f63af47ac000
Shaileshprajapati-dev <95132308+Shaileshprajapati-dev@users.noreply.github.com>
2025-07-19 10:31:54 +0530

    Add files via upload

---
+++ Creata _Calculator_Demo_Application/Creata _Calculator_Demo_Application/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creata__Calculator_Demo_Application
+{
+    internal class Program
+    {
+
+        public static void Addition(int a, int b)
+        {
+           int result = a + b;
+           Console.WriteLine("Addition Result: {0} " + result);
+        }
+
+        public static void Subtraction(int a, int b)
+        {
+            int result = a - b;
+            Console.WriteLine("Subtraction Result: {0} " + result);
+        }
+
+        public static void Multplication(int a, int b)
+        {
+            int result = a * b;
+            Console.WriteLine("Multplication Result: {0} " + result);
+        }
+
+
+        public static void Division(int a, int b)
+        {
+            int result = a / b;
+            Console.WriteLine("Division Result: {0} " + result);
+        }
+
+        static void Main(string[] args)
+        {
+            // how to use the Creata Calculator library
+            //method 1: using the Creata Calculator library
+            //decimal result = Creata_Calculator.Calculator.Add(5.5m, 2.2m);
+            //conditional compilation to use the Creata Calculator library
+
+
+            Console.WriteLine("Enter First Number");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Secound Number");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Operator (+, -, *, /)");
+            string op = Console.ReadLine();
+
+            if(op == "+")
+            {
+                Addition(num1, num2);
+            }
+            else if(op == "-")
+            {
+                Subtraction(num1, num2);
+            }
+            else if(op == "*")
+            {
+                Multplication(num1, num2);
+            }
+            else if(op == "/")
+            {
+                Division(num1, num2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operator");
+            }
+            Console.ReadLine();
+        }
+    }
+}
