5485624510822b11802b9738731ffab94a7c9902
Shaileshprajapati-dev <95132308+Shaileshprajapati-dev@users.noreply.github.com>
2025-07-19 12:56:58 +0530

    Add files via upload

---
+++ Constructor_IN_Inheritence/Constructor_IN_Inheritence/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_IN_Inheritence
+{
+    class BaseClass
+    {
+        public BaseClass()
+        {
+            Console.WriteLine("This is a BaseClass Constructor");
+        }
+    }
+
+    class DerivedClass : BaseClass
+    {
+        public DerivedClass()
+        {
+            Console.WriteLine("This is a DerivedClass Constructor");
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass dc= new DerivedClass();
+            Console.WriteLine("This is the Main Method");
+            Console.ReadLine();
+        }
+    }
+}
