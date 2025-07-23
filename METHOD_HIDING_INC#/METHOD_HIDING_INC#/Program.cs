7d28a8d104d387369f5c065aec9c63b4863b93b9
Shaileshprajapati-dev <95132308+Shaileshprajapati-dev@users.noreply.github.com>
2025-07-23 14:50:04 +0530

    Add files via upload

---
+++ METHOD_HIDING_INC#/METHOD_HIDING_INC#/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace METHOD_HIDING_INC_
+{
+    class parent
+    { 
+      public void Method()
+        {
+            Console.WriteLine("Parent class Method");
+        }
+    }
+
+    class child : parent
+    {
+        public new void Method()
+        {
+            Console.WriteLine("Child class Method");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            parent p = new parent();
+            p.Method(); // Calls Parent Method
+            child c = new child();
+            c.Method(); // Calls Child Method
+            parent pc = new child();
+            pc.Method(); // Calls Parent Method due to method hiding
+            Console.ReadLine(); 
+        }
+    }
+}
