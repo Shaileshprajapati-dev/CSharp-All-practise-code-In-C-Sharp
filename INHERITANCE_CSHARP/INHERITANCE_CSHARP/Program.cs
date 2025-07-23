dedec0a80c4282d84ecf5c3ca40b339b1f62c82c
Shaileshprajapati-dev <95132308+Shaileshprajapati-dev@users.noreply.github.com>
2025-07-23 13:06:46 +0530

    Add files via upload

---
+++ INHERITANCE_CSHARP/INHERITANCE_CSHARP/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INHERITANCE_CSHARP
+{
+    class visitingEmployee : Employee
+    {
+        public int visitingSalary;
+        public int visitingHours;
+    }
+    
+
+    class permanentEmployee : Employee
+    {
+        public int permanentSalary;
+        public int permanentHours;
+    }
+    
+
+    class Employee
+    {
+        public int Empid;
+        public string EmpName;
+        public string EmpAge;
+        public string EmpContactNo;
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            permanentEmployee pEmp = new permanentEmployee();
+            pEmp.Empid = 1;
+            pEmp.EmpName = "John Doe";
+            pEmp.EmpAge = "30";
+            pEmp.EmpContactNo = "123-456-7890";
+            pEmp.permanentSalary = 50000;
+            pEmp.permanentHours = 40;
+            Console.WriteLine($"Permanent Employee: {pEmp.EmpName}, ID: {pEmp.Empid}, Age: {pEmp.EmpAge}, Contact: {pEmp.EmpContactNo}, Salary: {pEmp.permanentSalary}, Hours: {pEmp.permanentHours}");
+            visitingEmployee vEmp = new visitingEmployee();
+            vEmp.Empid = 2;
+            vEmp.EmpName = "Jane Smith";
+            vEmp.EmpAge = "28";
+            vEmp.EmpContactNo = "987-654-3210";
+            vEmp.visitingSalary = 30000;
+            vEmp.visitingHours = 20;
+            Console.WriteLine($"Visiting Employee: {vEmp.EmpName}, ID: {vEmp.Empid}, Age: {vEmp.EmpAge}, Contact: {vEmp.EmpContactNo}, Salary: {vEmp.visitingSalary}, Hours: {vEmp.visitingHours}");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ INHERITANCE_CSHARP/INHERITANCE_CSHARP/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("INHERITANCE_CSHARP")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("INHERITANCE_CSHARP")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("df490e35-18e7-4ddd-a6f0-cc0448865f10")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
