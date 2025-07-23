cce46a2b94ec27ace3b38ea09b0dbde6a9ee3aae
Shaileshprajapati-dev <95132308+Shaileshprajapati-dev@users.noreply.github.com>
2025-07-23 13:08:57 +0530

    Add files via upload

---
+++ VAR_AND_DYNAMIC/VAR_AND_DYNAMIC/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VAR_AND_DYNAMIC
+{
+    internal class Program
+
+
+    {
+        public static int show()
+        {
+            return 0;
+        }
+
+        static void Main(string[] args)
+        {
+            int number = 10;
+            var variableNumber = 20; // Implicitly typed variable
+             Console.WriteLine($"Number: {number}, Variable Number: {variableNumber}");
+
+            dynamic dynamicVariable = "Hello, World!"; // Dynamic type
+            Console.WriteLine($"Dynamic Variable: {dynamicVariable}");
+
+
+            Console.ReadLine(); 
+        }
+    }
+}
