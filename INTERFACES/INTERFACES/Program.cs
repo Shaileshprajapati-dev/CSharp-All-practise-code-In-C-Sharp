1779706b8f32b1e2e64a6ce19f2e9a4d4f961838
Shaileshprajapati-dev <95132308+Shaileshprajapati-dev@users.noreply.github.com>
2025-07-24 11:38:24 +0530

    Add files via upload

---
+++ INTERFACES/INTERFACES/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INTERFACES
+{
+    interface IEmployee
+    { 
+       void showDetails();
+
+    }
+
+    class Employee : IEmployee
+    {
+        public void showDetails()
+        {
+            Console.WriteLine("Employee details are displayed.");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEmployee employee = new Employee();
+            employee.showDetails();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+            Console.ReadLine();
+        }
+    }
+}
