8e0dc53bf0ffdcb4b654ce39d429afeb180c35ad
Shaileshprajapati-dev <95132308+Shaileshprajapati-dev@users.noreply.github.com>
2025-08-02 17:28:59 +0530

    Add files via upload

---
+++ Static_Local_Function/Static_Local_Function/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Static_Local_Function
+{
+    class Program
+    {
+        static void Main()
+        {
+            int val = 10;
+
+            // Regular Local Function
+            void ShowRegular()
+            {
+                Console.WriteLine("Regular Function: " + val); // val को access कर सकता है
+            }
+
+            // Static Local Function
+            static void ShowStatic(int v)
+            {
+                Console.WriteLine("Static Function: " + v); // val को parameter से लेना पड़ेगा
+            }
+
+            ShowRegular();
+            ShowStatic(val);
+        }
+    }
+}
