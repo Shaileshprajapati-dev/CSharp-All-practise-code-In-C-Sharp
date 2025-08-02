ad38325f6d8a3fc4e925fc26b510b2b2cefe85a2
Shaileshprajapati-dev <95132308+Shaileshprajapati-dev@users.noreply.github.com>
2025-08-02 17:35:03 +0530

    Add files via upload

---
+++ FileScope_NameSpace/FileScope_NameSpace/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace FileScope_NameSpace
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            User user = new User
+            {
+                Id = 1,
+                Name = "Shailesh",
+                Email = "shailesh@example.com"
+            };
+
+            Console.WriteLine($"ID: {user.Id}, Name: {user.Name}, Email: {user.Email}");
+        }
+    }
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+
+
+}
