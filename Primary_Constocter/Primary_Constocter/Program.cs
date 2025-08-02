e23e352b55966689b414e3d3a7044727d438bd93
Shaileshprajapati-dev <95132308+Shaileshprajapati-dev@users.noreply.github.com>
2025-08-02 17:23:37 +0530

    Add files via upload

---
+++ Primary_Constocter/Primary_Constocter/Program.cs
@@ -0,0 +1,19 @@
+﻿
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Student student = new Student(1, "Shailesh Prajapati", 20);
+              student.Display();
+    }
+    }
+
+    class Student(int Id, string Name, int Age)
+   {
+    
+     public void Display()
+        {
+            Console.WriteLine($"Id: {Id}, Name: {Name}, Age: {Age}");
+    }
+ 
+  }
